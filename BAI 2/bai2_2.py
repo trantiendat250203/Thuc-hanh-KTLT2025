6e5e69d8aeca20d73791e1e90078bb6c05fb83e3
trantiendat250203 <trantiendat250203@gmail.com>
2025-03-07 12:53:28 +0700

    Add files via upload

---
+++ BAI 2/bai2_2.py
@@ -0,0 +1,11 @@
+import math
+x1 = int(input("Enter x1: "))
+y1 = int(input("Enter y1: "))
+x2 = int(input("Enter x2: "))
+y2 = int(input("Enter y2: "))
+d1 = (x2 -x1) ** 2
+d2 = (y2- y1) ** 2
+res = math.sqrt(d1 + d2)
+print("Distance between two points: ",res)
+print("sinh vien: Tran Van Tien Dat")
+print("Mssv: 235752020710004")
---
+++ BAI 2/bai2_4.py
@@ -0,0 +1,7 @@
+i=1;
+for j in range(2,10):
+    print("i:",i,"j",j)
+    print(i,"/",j)
+    print (i/j);
+print ("sinh vien: Tran Van Tien Dat")
+print ("Mssv: 235752020710004")
