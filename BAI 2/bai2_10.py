61f646498e1b22b1cd797878b75d2ae3e615216b
trantiendat250203 <trantiendat250203@gmail.com>
2025-03-07 12:56:55 +0700

    Add files via upload

---
+++ BAI 2/bai2_10.py
@@ -0,0 +1,7 @@
+a="hi i am python programmer"
+b=a.split()
+print (b)
+c=" ".join(b)
+print(c)
+print ("sinh vien: Tran Van Tien Dat")
+print ("Mssv: 235752020710004") 
---
+++ BAI 2/bai2_11.py
@@ -0,0 +1,10 @@
+l=[1,'python',4,7]
+k=['cse',2,'guntur',8]
+m=[]
+m.append(l);
+m.append(k);
+print(m)
+d={1:l,2:k,'combine_list':m}
+print(d)
+print ("sinh vien: Tran Van Tien Dat")
+print ("Mssv: 235752020710004")
