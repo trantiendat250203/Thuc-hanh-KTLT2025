bec5490ba6799b35e3beee952281f60c69ee7d26
trantiendat250203 <trantiendat250203@gmail.com>
2025-05-16 07:03:18 +0700

    Delete BAI 7/bai7_10.py

--- BAI 7/bai7_10.py
+++
@@ -1,9 +0,0 @@
-def find_longest_words(text):
-    words = text.split()
-    max_length = len(max(words, key=len))
-    longest_words = [word for word in words if len(word) == max_length]
-    return longest_words
-text = """ngo xuan hieu sinh vien nganh tu dong hoa vien ky thuat cong nghe truong dai hoc vinh"""
-print(find_longest_words(text))
-print("sinh vien: Tran Van Tien Dat")
-print("Mssv: 235752020710004")
